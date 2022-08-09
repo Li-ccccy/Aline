b5a1e6087a230e0e94ce3c0bcd7d536f685ede54
李超宇 <153879482@qq.com>
2022-08-09 16:21:11 +0800

    测试

--- Alien/components/Button/index.tsx
+++ Alien/components/Button/index.tsx
@@ -2,6 +2,6 @@ import { defineComponent } from "vue";
 import { Button } from "ant-design-vue";
 export default defineComponent({
   render() {
-    return <Button>测试</Button>;
+    return <div>测试</div>;
   },
 });
