243e5a87d1514ba05a4ed8ec06fc2f52fffd5a60
李超宇 <153879482@qq.com>
2022-08-10 14:27:15 +0800

    init(架构)：初始化

--- packages/Warehouse/components.d.ts
+++ packages/Warehouse/components.d.ts
@@ -9,6 +9,6 @@ declare module "@vue/runtime-core" {
   export interface GlobalComponents {
     AButton: typeof import("ant-design-vue/es")["Button"];
     HelloWorld: typeof import("./src/components/HelloWorld.vue")["default"];
-    Test: typeof import("./src/tset")["default"];
+    Test: typeof import("./src/components/tset")["default"];
   }
 }
--- packages/Warehouse/src/App.vue
+++ packages/Warehouse/src/App.vue
@@ -2,7 +2,7 @@
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
 import HelloWorld from "./components/HelloWorld.vue";
-import Test from "./tset";
+import Test from "./components/tset";
 import workspace from "@alien/components/Button";
 </script>
 
