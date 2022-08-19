bd560e5ca2d4774b8f4a3d3e2b40f3c7aa6ed9e0
李超宇 <153879482@qq.com>
2022-08-19 10:48:01 +0800

    feat: 🔥新增commitlint提交规范

--- start/index.js
+++ start/index.js
@@ -4,7 +4,7 @@ import shell from "shelljs";
 const init = async () => {
   // 获取用户所选择的项目
   let { packages } = await interactive;
-  // 执行命令--- 启动项目
+  // 执行命令--- 启动项目----
   shell.exec(`cd packages/${packages} && pnpm run dev --host`, {
     stdio: "inherit",
     shell: true,
